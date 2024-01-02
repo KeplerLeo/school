70de11e2b09bf2e74d90a4b5b52f1ed633c8e4a0
Leonardo Kepler <leokepler@gmail.com>
2024-01-02 15:32:21 -0300

    corrigindo conexão do banco no container

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,14 +1,17 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { Sequelize } from 'sequelize'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import dotenv from 'dotenv/config.js'
 
-const dbName = 'SCHOOL'
-const dbUser = 'root'
-const dbPassword = '123456'
-const dbHost = 'localhost'
+const dbName = process.env.DB_NAME || 'SCHOOL'
+const dbUser = process.env.DB_USER || 'root'
+const dbPassword = process.env.DB_PASSWORD || '123456'
+const dbHost = process.env.DB_HOST || 'localhost'
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   dialect: 'mysql',
-  host: dbHost,
-  port: 3002
+  host: dbHost
 })
 
 export default sequelize
